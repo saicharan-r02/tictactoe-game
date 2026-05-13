af254b998bb0d48b811daa5a82ce418b6b1cdccb
saicharan-o <saicharanr227@gmail.com>
2026-05-13 15:54:25 +0530

    changes in market file

--- market.py
+++ market.py
@@ -1,6 +1,8 @@
 import matplotlib.pylab as plt
+
 a=[120,79,80,89,106,130,149,114]
 b=[119,110,128,91,109,139,116,123,70]
+
 plt.xlabel("Market Price Range")
 plt.ylabel("Frequency")
 plt.title("Share Market Price Distribution")
