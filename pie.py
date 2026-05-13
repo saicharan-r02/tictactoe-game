570fd9ca687e2df3ae682fbc7e46371c30261276
saicharan-o <saicharanr227@gmail.com>
2026-05-13 15:54:56 +0530

    updated the file

--- pie.py
+++ pie.py
@@ -1,6 +1,8 @@
 import matplotlib.pylab as plt
+
 b=[1190,810,528,691,1009,889,416,123,70]
 a=["Electronics","Clothing","Groceries","Home Appliances","Furniture","Footwear","Books","Toys","Others"]
+
 plt.title("Category-wise Distribution of Values")
 plt.axis("equal")
 plt.pie(b,labels=a,autopct="%0.1f%%",radius=1.06,explode=[0,0.1,0.1,0,0,0,0.1,0.1,0.2])
