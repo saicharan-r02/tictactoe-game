ddbfe9273b68b646f2f09276632ae5996fd42fbd
saicharan-o <saicharanr227@gmail.com>
2026-05-13 15:56:01 +0530

    changes in sea file

--- sea.py
+++ sea.py
@@ -2,9 +2,11 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd 
+
 plt.style.use("classic")
 d=sn.load_dataset("car_crashes")
 print(d.head())
+
 sn.relplot(x="total",y="speeding",kind="scatter",data=d)
 sn.relplot(x="total",y="speeding",hue="abbrev",data=d)
 sn.relplot(x="total",y="speeding",hue="abbrev",style="abbrev",data=d)
