7abea80639eaab51ca109e78cd2810276fb43284
saicharan-o <saicharanr227@gmail.com>
2026-05-13 15:56:30 +0530

    updates in file

--- sea1.py
+++ sea1.py
@@ -2,9 +2,11 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd 
+
 plt.style.use("seaborn-v0_8")
 d=sn.load_dataset("diamonds")
 print(d.head())
+
 sn.relplot(x="carat",y="price",kind="line",data=d)
 sn.relplot(x="price",y="price",hue="cut",data=d)
 sn.relplot(x="carat",y="price",hue="cut",style="cut",data=d)
