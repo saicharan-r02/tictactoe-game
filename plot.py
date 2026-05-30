fa7b530743cdfafd4483b922f21b5e1c4287c3cb
saicharan <saicharanr227@gmail.com>
2026-05-30 22:59:16 +0530

    Correct ylabel typo and format code
    Fix typo in ylabel and improve code formatting.

--- plot.py
+++ plot.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 
+
 a=[1,3,5,7,9,11,13,15,18]
 b=[39,50,48,61,37,49,56,29,30]
 
 plt.xlabel("Days") 
 plt.ylabel("Tempuratur")
 plt.title("Data")
+
 plt.scatter(a,b,color="black",linewidth=2)
 plt.show()
