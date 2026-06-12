6de26ae9d395d885a0614cf027d74f58b32a7c18
saicharan <saicharanr227@gmail.com>
2026-06-12 22:14:53 +0530

    Implement pie chart visualization with saving feature
    Added pie chart visualization and saved the output as a PNG file.

--- pie.py
+++ pie.py
@@ -5,7 +5,9 @@ a=["Electronics","Clothing","Groceries","Home Appliances","Furniture","Footwear"
 
 plt.title("Category-wise Distribution of Values")
 plt.axis("equal")
+
 plt.pie(b,labels=a,autopct="%0.1f%%",radius=1.06,explode=[0,0.1,0.1,0,0,0,0.1,0.1,0.2])
 plt.savefig(r"C:\Users\saich\OneDrive\Desktop\python\tictactoe\piechart.png",bbox_inches="tight",pad_inches=4,transparent=True)
+
 plt.show()
 
