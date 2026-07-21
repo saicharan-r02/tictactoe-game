30b83351f0b4c36befb430c973a5b3830f343765
saicharan-r02 <saicharanr227@gmail.com>
2026-07-21 23:16:56 +0530

    Changes in market file

--- market.py
+++ market.py
@@ -1,11 +1,11 @@
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 
-a=[120,79,80,89,106,130,149,114]
-b=[119,110,128,91,109,139,116,123,70]
+a = [120, 79, 80, 89, 106, 130, 149, 114]
+b = [119, 110, 128, 91, 109, 139, 116, 123, 70]
 
 plt.xlabel("Market Price Range")
 plt.ylabel("Frequency")
 plt.title("Share Market Price Distribution")
-plt.hist([a,b],bins=[70,100,125,150],rwidth=0.80,color=["blue","red"],label=["Stock A", "Stock B"])
+plt.hist([a, b], bins=[70, 100, 125, 150], rwidth=0.80, color=["blue", "red"], label=["Stock A", "Stock B"])
 plt.legend()
 plt.show()
